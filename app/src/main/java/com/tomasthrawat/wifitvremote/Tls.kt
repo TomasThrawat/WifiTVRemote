c2d8e2035e3106941791d3d90938d3c007ecf7ed
TomasThrawat <tomastharwat873@gmail.com>
2026-09-19 11:32:11 +0300

    feat: rebuild as native modern Kotlin Wi-Fi TV remote

---
+++ app/src/main/java/com/tomasthrawat/wifitvremote/Tls.kt
@@ -0,0 +1,17 @@
+package com.tomasthrawat.wifitvremote
+import java.security.KeyStore
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
+object Tls{
+    fun context(id:ClientIdentity):SSLContext{
+        val ks=KeyStore.getInstance("PKCS12");ks.load(null,null);val pass="wifitv".toCharArray();ks.setKeyEntry("client",id.key,pass,arrayOf(id.cert))
+        val km=KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());km.init(ks,pass)
+        val trust=object:X509TrustManager{
+            override fun checkClientTrusted(c:Array<out X509Certificate>?,a:String?){}
+            override fun checkServerTrusted(c:Array<out X509Certificate>?,a:String?){}
+            override fun getAcceptedIssuers():Array<X509Certificate>=emptyArray()
+        }
+        return SSLContext.getInstance("TLS").apply{init(km.keyManagers,arrayOf<TrustManager>(trust),SecureRandom())}
+    }
+}
