12d04caf274c2b436d05d10c1def9c5ebaf542bf
TomasThrawat <tomastharwat873@gmail.com>
2026-09-26 13:26:23 +0300

    feat: redesign TV remote control UI

---
+++ app/src/main/java/com/tomasthrawat/wifitvremote/RemoteUiSpec.kt
@@ -0,0 +1,8 @@
+package com.tomasthrawat.wifitvremote
+
+object RemoteUiSpec {
+    val volumeControls = listOf("Vol +", "Mute", "Vol -")
+    val channelControls = listOf("Ch +", "Menu", "Ch -")
+    val quickActions = listOf("Back", "Home", "Power")
+    val bottomNavigation = listOf("Remote", "Apps", "Settings")
+}
